df9865fdf5d1569b92352a03b1eccaeb050b236c
AlexandreLon <alexandre.longordo@etu.unice.fr>
2020-11-13 22:09:29 +0100

    Update .env.example

--- routes/statistics/index.js
+++ routes/statistics/index.js
@@ -3,19 +3,16 @@ const express = require("express");
 const router = express.Router();
 var statsController = require("../../appengine/controller/StatsController.js");
 
-router.get(
-  "/",
-  handleExceptions(async (req, res) => {
-    let valueToReturn;
-    if (req.query["numberofusers"] !== undefined) {
-      valueToReturn = await statsController.getNumberOfUsers();
-    } else if (req.query["numberofpoi"] !== undefined) {
-      valueToReturn = await statsController.getNumberOfPoi();
-    } else {
-      valueToReturn = statsController.test();
-    }
-    res.status(200).json(valueToReturn);
-  })
-);
+router.get("/", handleExceptions(async (req, res) => {
+	let valueToReturn;
+	if (req.query["numberofusers"] !== undefined) {
+		valueToReturn = await statsController.getNumberOfUsers();
+	} else if (req.query["numberofpoi"] !== undefined) {
+		valueToReturn = await statsController.getNumberOfPoi();
+	} else {
+		valueToReturn = statsController.test();
+	}
+	res.status(200).json(valueToReturn);
+}));
 
 module.exports = router;
