b2084e147aec14af236845a8a50d37ec5e76d0d6
AlexandreLon <alexandre.longordo@etu.unice.fr>
2020-11-13 16:40:36 +0100

    Update model snake

--- middlewares/models/user.js
+++ middlewares/models/user.js
@@ -4,7 +4,7 @@ const Joi = require('joi');
 const schema = Joi.object({
     sha1: Joi.string().pattern(/^([A-Za-z0-9]{10})+$/).required(),
     email: Joi.string().email({ minDomainSegments: 2 }).required(),
-    user_status_changeDate: Joi.date().timestamp(),
+    user_status_change_date: Joi.date().timestamp(),
 });
 
 // return { error, value }
--- routes/user/index.js
+++ routes/user/index.js
@@ -1,72 +1,59 @@
-const { handleExceptions } = require("../../middlewares/error-handlers");
-const { ValidationError, NotFoundError } = require("../../middlewares/errors");
+const { handleExceptions } = require('../../middlewares/error-handlers');
+const { ValidationError, NotFoundError } = require('../../middlewares/errors');
 const { ensureIsAdmin } = require("../../middlewares/authorization");
 const validateUser = require("../../middlewares/models/user");
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const userSQL = require("../../database/models/user");
+const userSQL = require('../../database/models/user')
 
 // testing purpose for now
 const users = [];
 
-router.get(
-  "/",
-  handleExceptions(async (req, res) => {
-    userSQL
-      .getUser()
-      .then((users) => {
+router.get('/', handleExceptions(async (req, res) => {
+    userSQL.getUser().then(users => {
         res.status(200).json(users);
-      })
-      .catch((err) => {
-        res.status(400).send(err);
-      });
+    }).catch(err => {
+        res.status(400).send(err)
+    })
     // res.status(200).json(users);
-  })
-);
+}));
 
-router.post(
-  "/",
-  handleExceptions(async (req, res) => {
+router.get('/storage', handleExceptions(async (req, res) => {
+    res.status(200).json({
+        user: process.env.PGSQL_USER,
+        host: process.env.PGSQL_HOST,
+        database: process.env.PGSQL_DATABASE,
+        password: process.env.PGSQL_PASSWORD,
+        port: process.env.PGSQL_PORT
+    })
+}));
+
+router.post('/', handleExceptions(async (req, res) => {
     const user = req.body;
     const { error } = validateUser(user);
     if (error) {
-      throw new ValidationError(`User does not match schema ${user}`, error);
+        throw new ValidationError(`User does not match schema ${user}`, error);
     }
     // users.push(user);
-    userSQL
-      .createUser(user)
-      .then((resUser) => {
+    userSQL.createUser(user).then(resUser => {
         res.status(201).json(resUser);
-      })
-      .catch((err) => {
-        res.status(400).send(err);
-      });
-  })
-);
+    }).catch(err => {
+        res.status(400).send(err)
+    })
+}));
 
-router.put(
-  "/:sha1",
-  ensureIsAdmin,
-  handleExceptions(async (req, res) => {
-    const sha1 = req.params.sha1;
+router.put('/:sha1', ensureIsAdmin, handleExceptions(async (req, res) => {
+    const sha1 = req.params.sha1
     const user = await userSQL.getUserSha1(sha1);
-
     if (!user) {
-      throw new NotFoundError(`User not found with sha1 ${sha1}`);
+        throw new NotFoundError(`User not found with sha1 ${sha1}`);
     }
-    user.user_status_change_date = new Date()
-      .toISOString()
-      .slice(0, 19)
-      .replace("T", " ");
-    userSQL
-      .putUser(sha1, user)
-      .then((resUser) => {
+    user.user_status_change_date = new Date().toISOString().slice(0, 19).replace('T', ' ');
+    userSQL.putUser(sha1, user).then(resUser => {
         res.status(201).json(resUser);
-      })
-      .catch((err) => {
-        res.status(400).send(err);
-      });
-  })
-);
+    }).catch(err => {
+        res.status(400).send(err)
+    })
+}));
 
 module.exports = router;
